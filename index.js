66aa9a353cfbf1d8ed844e3732777da790153369
Turbo Biscuit <55338035+TurboBiscuit@users.noreply.github.com>
2020-04-16 21:43:48 -0400

    added auto availible sub detection

--- index.js
+++ index.js
@@ -24,40 +24,48 @@ app.get("/submittoken", (req, res) => {
     res.send("<script>setTimeout(function(){close()},5000)</script> You can close this page now")
     term("Got Access Token\n")
     var full_token = "Bearer " + req.query.access_token
-    term.white("Subreddit you want to broadcast to: ").inputField(
-        function (error, input) {
-            var subreddit = input
-            term.white("\nStream title: ").inputField(
-                function (error, input) {
-                    var streamtitle = input
-                    console.log("\nSending Data To Reddit")
-                    request({
-                        url: `https://strapi.reddit.com/r/${subreddit}/broadcasts?title=${streamtitle}`,
-                        headers: {
-                            'User-Agent': 'Project SnooKey/0.1',
-                            'Authorization': full_token
-                        },
-                        method: "POST"
-                    }, (error, response, body) => {
-                        console.log("Response From Reddit")
-                        if (response.statusCode == 200) {
-                            var data = JSON.parse(body)
-                            console.log("STREAM URL: rtmp://ingest.redd.it/inbound/")
-                            console.log("\nSTREAM KEY: " + data.data.streamer_key)
-                            console.log("\nYOU ARE LIVE: " + data.data.post.outboundLink.url)
-                            process.exit(0)
-                        } else {
-                            console.log("\nError Code: " + response.statusCode)
-                            try {
-                                var data = JSON.parse(body)
-                                if (data.status) console.log("Error Status: " + data.status)
-                            } catch (e) {}
-                            process.exit(0)
-                        }
+    request("https://strapi.reddit.com/recommended_viewer_subreddits", (err1, response1, body1) => {
+        var history = JSON.parse(body1).data
 
-                    })
-                }
-            )
-        }
-    );
+        term.white("Subreddit you want to broadcast to: ").inputField({
+                history: history,
+                autoComplete: history,
+                autoCompleteHint: true
+            },
+            function (error, input) {
+                var subreddit = input
+                term.white("\nStream title: ").inputField(
+                    function (error, input) {
+                        var streamtitle = input
+                        console.log("\nSending Data To Reddit")
+                        request({
+                            url: `https://strapi.reddit.com/r/${subreddit}/broadcasts?title=${streamtitle}`,
+                            headers: {
+                                'User-Agent': 'Project SnooKey/0.1',
+                                'Authorization': full_token
+                            },
+                            method: "POST"
+                        }, (error, response, body) => {
+                            console.log("Response From Reddit")
+                            if (response.statusCode == 200) {
+                                var data = JSON.parse(body)
+                                console.log("STREAM URL: rtmp://ingest.redd.it/inbound/")
+                                console.log("\nSTREAM KEY: " + data.data.streamer_key)
+                                console.log("\nYOU ARE LIVE: " + data.data.post.outboundLink.url)
+                                process.exit(0)
+                            } else {
+                                console.log("\nError Code: " + response.statusCode)
+                                try {
+                                    var data = JSON.parse(body)
+                                    if (data.status) console.log("Error Status: " + data.status)
+                                } catch (e) {}
+                                process.exit(0)
+                            }
+
+                        })
+                    }
+                )
+            }
+        );
+    })
 })
